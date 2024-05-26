0bc7764166f12fc43505bea126bd088361fc89c1
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2024-05-26 16:59:46 +0200

    added reduxjs toolkit. added countries and public holidays api changes

---
+++ src/api/index.ts
@@ -0,0 +1,25 @@
+import { API_BASE_URL } from '../constants';
+import { IPublicHolidaysRequestPayload } from './types';
+
+export const getCountries = async () => {
+    return await fetch(`${API_BASE_URL}/Countries`)
+        .then(data => data.json())
+        .catch(error => {
+            return Promise.reject(error);
+        });
+};
+
+export const getPublicHolidays = async ({
+    countryIsoCode,
+    languageIsoCode,
+    validFrom,
+    validTo,
+}: IPublicHolidaysRequestPayload) => {
+    const publicHolidaysURL = `${API_BASE_URL}/PublicHolidays?countryIsoCode=${countryIsoCode}
+    &languageIsoCode=${languageIsoCode}&validFrom=${validFrom}&validTo=${validTo}`;
+    return await fetch(publicHolidaysURL)
+        .then(data => data.json())
+        .catch(error => {
+            return Promise.reject(error);
+        });
+};
---
+++ src/api/types.ts
@@ -0,0 +1,33 @@
+export enum PublicHolidayQuality {
+    MANDATORY = 'Mandatory',
+}
+
+export enum HolidayType {
+    PUBLIC = 'Public',
+}
+
+export interface ICountry {
+    isoCode: string;
+    name: Array<{ language: string; text: string }>;
+    officialLanguages: string[];
+}
+
+export interface IPublicHolidaysRequestPayload {
+    countryIsoCode: string;
+    languageIsoCode?: string;
+    validFrom: string;
+    validTo: string;
+    subdivisionCode?: string;
+}
+
+export interface IPublicHoliday {
+    comment: Array<{ language: string; text: string }>;
+    endDate: string;
+    id: string;
+    name: Array<{ language: string; text: string }>;
+    nationwide: boolean;
+    quality: PublicHolidayQuality;
+    startDate: string;
+    subdivisions: Array<{ code: string; shortName: string }>;
+    type: HolidayType.PUBLIC;
+}
